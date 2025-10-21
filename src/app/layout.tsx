02f0b45199c2a87b0982bca5e87f9095616ef129
satan0 <mhrjan0@gmail.com>
2025-10-21 15:09:10 +0545

    New

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,7 +72,6 @@ export const metadata: Metadata = {
     verification: {
         google: "your-google-verification-code",
         yandex: "your-yandex-verification-code",
-        bing: "your-bing-verification-code",
     },
     category: "technology",
 };
